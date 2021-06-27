658298da078fc2fe430590307fcf53f892dec38a
thongtm2 <minhthong494@gmail.com>
2021-06-26 20:57:47 -0700

    fix too long render

--- lib/crawler_demo_web/controllers/page_controller.ex
+++ lib/crawler_demo_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule CrawlerDemoWeb.PageController do
   use CrawlerDemoWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    url = "https://shopee.vn/apple_flagship_store"
+    data = Crawler.parse_item(url)
+    render(conn, "index.html", data: data)
   end
 end
